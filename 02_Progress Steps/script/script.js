6744ba72739db977da87c5fe07031df01eced1a2
Anchal Singh <anchals0915@gmail.com>
2023-06-12 23:48:51 +0530

    intial commit

---
+++ 02_Progress Steps/script/script.js
@@ -0,0 +1,35 @@
+const progress = document.getElementById("progress") ;
+const prev = document.getElementById('prev') ;
+const next = document.getElementById('next') ;
+const circles = document.querySelectorAll('.circle') ;
+
+let currentActive = 1;
+next.addEventListener('click', () => {
+    currentActive++;
+    if(currentActive > circles.length) {
+        currentActive = circles.length ;
+    }
+    update() ;
+})
+prev.addEventListener('click', () => {
+    currentActive++;
+    if(currentActive < 1) {
+        currentActive = 1 ;
+    }
+    update() ;
+})
+
+function update() {
+    //Update Active class in circle 
+    circles.forEach ( (circle,index) => {
+        if(index <= currentActive ) {
+            circles.classList.add('active')
+        }else {
+            circles.classList.remove('active')
+        }
+    } )
+    // update the progress bar
+
+    // Change the buttin enabled and disabled state 
+
+}
